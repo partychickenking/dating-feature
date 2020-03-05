d38cab67a7df7641ec4547e8675f2edf5c8d2723
partychickenking <partychickenking@gmail.com>
2020-03-05 16:26:24 +0100

    Added html pages, whole screenflow

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const cc = require('camelcase');
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+console.log("check");
+
+var register2 = document.querySelector(".next-btn");
 
 
-console.log(cc('dit is een flinke test'));
+
+var next = function() {
+    register2.classList.toggle("nextregister2");
+};
+
+register2.addEventListener("click", next);
+
