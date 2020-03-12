479e0654de6ea3c195d991447a3ec954259424e2
partychickenking <partychickenking@gmail.com>
2020-03-12 17:33:36 +0100

    Added mongoDB

--- server.js
+++ server.js
@@ -3,12 +3,86 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const mongo = require('mongodb').MongoClient;
+const assert = require('assert');
+const bodyParser = require('body-parser');
 
-const movies = {
-    title: 'Titanic',
-    regisseur: 'James Cameron',
-    nominaties: '71'
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://Inju:Kato1234!@moa-lfz7p.mongodb.net/test?retryWrites=true&w=majority";
+
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.3/api/MongoClient.html for more details
+     */
+    const client = new mongo(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
 }
+
+main().catch(console.error);
+
+/**
+ * Print the names of all available databases
+ * @param {mongo} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+app.get('/get-data', function(req, res, next) {
+    var resultArray = [];
+    mongo.connect(uri, function(err, db) {
+        assert.equal(null, err);
+        var cursor = db.collection('register').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err)
+            resultArray.push(doc);
+        }, function() {
+            db.close();
+            res.render('index', {items: resultArray});
+        });
+    });
+});
+
+app.post('/insert', function(req, res, next) {
+    var item = {
+        username: req.body.username,
+        email: req.body.email,
+        ww: req.body.ww
+    };
+
+    mongo.connect(uri, function(err, db) {
+        assert.equal(null, err);
+        db.collection('register').insertOne(item, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item inserted');
+            db.close();
+        });
+
+    });
+});
+
+
+
 
 //
 app.use('/static', express.static('static'));
@@ -28,8 +102,7 @@ app.get('/register', (req, res) => res.sendfile(path.join(__dirname + '/static/r
 
 //dynamische pagina waarbij je gebruikt maakt van objects
 app.get('/dynamic', (req, res) => {
-    res.render('index',{data : movies})
+    res.render('index', { data: movies })
 })
 
-console.log(movies)
 app.listen(port, () => console.log(cc(`Example app listening on port ${port}!`)));
